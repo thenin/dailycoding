30c0195414615da86851f4f8b4d6fb786e98ee81
kguryanov <kguryanocv@gmail.com>
2019-05-11 23:36:03 +0300

    final with dataprovider

--- adds/src/main/java/org/thenin/dailycoding/adds/addsArray.java
+++ adds/src/main/java/org/thenin/dailycoding/adds/addsArray.java
@@ -1,21 +1,35 @@
 package org.thenin.dailycoding.adds;
 
+import org.testng.log4testng.Logger;
+
 import java.util.Arrays;
 
 public class addsArray {
 
+    private static Logger log = Logger.getLogger(addsArray.class);
     /*Sort the array asc, calculate sums from left and right parts */
+
     public static boolean checkSum(int[] values, int sum){
+        log.debug("Start calc.......");
+        boolean result = false;
         Arrays.sort(values);
         for (int i=0, j=values.length-1; i<j;){
-            int result = values[i]+values[j];
-            if (result<sum)
+            int resSum = values[i]+values[j];
+            log.debug(String.format("%d + %d = %d",values[i],values[j],resSum));
+            if (resSum<sum)
                 i++;
-            else if (result>sum)
+            else if (resSum>sum)
                 j--;
-            else
-                return true;
+            else {
+                result = true;
+                break;
+            }
         }
-        return false;
+        log.debug("Done calc.......");
+        return result;
+
     }
+
+
+
 }
