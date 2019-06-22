de13db991276c1f311dd80421d6d9375b29fb802
kguryanov <kguryanocv@gmail.com>
2019-06-22 05:01:47 +0300

    readmy minor change

--- stripe/day4/src/main/java/org/thenin/dailycoding/google/day4/Solution.java
+++ stripe/day4/src/main/java/org/thenin/dailycoding/google/day4/Solution.java
@@ -3,14 +3,49 @@ package org.thenin.dailycoding.google.day4;
 import java.util.Arrays;
 
 class Solution {
+//    Illegal iplementation - ignoring the requirement for "in linear time and constant space"
+//    Initial array can not be sorted
+//    Ahh, it would be so much easier, right?
     static int findMissingInt(int[] array) {
+//        Illegal for this assignment
         Arrays.sort(array);
         System.out.println(Arrays.toString(array));
-        for (int i = 0; i < array.length-1; i++) {
-            if (array[i]>=0 && (array[i+1]-array[i]>1)){
-                return array[i]+1;
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] >= 0 && (array[i + 1] - array[i] > 1)) {
+                return array[i] + 1;
             }
         }
+        return array[array.length - 1] + 1;
+    }
+
+//    Create an array of size equal to the max number in the initial array
+//    Unassigned values in primitive int[] are defaulted to 0
+//    Ignore values less than 1 (we need positive values only)
+//    Assign values to array indices, e,g, tmp[15]=15
+//    First array value to be equal to 0 is out first missing positive integer
+//    If none, it should be the next after the last array index, e.g. the array length
+    static int findMissingIntPainfully(int[] array) {
+        System.out.println(Arrays.toString(array));
+        int[] tmp = new int[calcArrayMaxLen(array)];
+        for (int i : array) {
+            if (i > 0)
+                tmp[i]=i;
+        }
+
+        for (int i = 1; i < tmp.length-1 ; i++) {
+            if (tmp[i]==0)
+                    return i;
+        }
+        return tmp.length;
+    }
+
+//    Calculate the length on the temp array
+    private static int calcArrayMaxLen(int[] array){
+//      Utility function, sort can be used here
+        Arrays.sort(array);
+        System.out.println(array[array.length-1]);
+//      Getting the appropriate new temp array size, e,g for [0,3,-1,15] result would
+//      new array indicies will be [0..15] with array.length=16
         return array[array.length-1]+1;
     }
 }
--- stripe/day4/src/test/java/org/thenin/dailycoding/google/day4/SimpleTest.java
+++ stripe/day4/src/test/java/org/thenin/dailycoding/google/day4/SimpleTest.java
@@ -4,11 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.thenin.dailycoding.google.day4.Solution.findMissingInt;
+import static org.thenin.dailycoding.google.day4.Solution.findMissingIntPainfully;
 
 public class SimpleTest {
     @Test
     public void doTest(){
         Assert.assertEquals(findMissingInt(new int[]{3, 4, -1, 1}),2);
         Assert.assertEquals(findMissingInt(new int[]{1, 2, 0}),3);
+    }
+
+    @Test
+    public void doTestPainfully(){
+        Assert.assertEquals(findMissingIntPainfully(new int[]{3, 4, -1, 1}),2);
+        Assert.assertEquals(findMissingIntPainfully(new int[]{1, 2, 0}),3);
     }
 }
